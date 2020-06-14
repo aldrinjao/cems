1b59d2e8730dc218064654a6e5778299f6c6aa09
sarai <aldrinhao@gmail.com>
2020-06-14 17:37:04 +0800

    complete

---
+++ data/coverage.js
@@ -0,0 +1,172 @@
+var coverage = [
+ {
+   "Area": "General Santos City",
+   "lat": 6.112331,
+   "lng": 125.1717145
+ },
+ {
+   "Area": "Pigcawayan, Cotabato",
+   "lat": 7.2791609,
+   "lng": 124.4242697
+ },
+ {
+   "Area": "Sultan Mastura, Maguindanao",
+   "lat": 7.3031726,
+   "lng": 124.2812604
+ },
+ {
+   "Area": "Buldon, Maguindanao",
+   "lat": 7.510727,
+   "lng": 124.3694892
+ },
+ {
+   "Area": "Barira, Maguindanao",
+   "lat": 7.4708165,
+   "lng": 124.3563569
+ },
+ {
+   "Area": "Cotabato City",
+   "lat": 7.2188133,
+   "lng": 124.196953
+ },
+ {
+   "Area": "Matanog, Maguindanao",
+   "lat": 7.4708165,
+   "lng": 124.2590444
+ },
+ {
+   "Area": "Indanan, Sulu",
+   "lat": 5.9728882,
+   "lng": 120.9702286
+ },
+ {
+   "Area": "Bongao, Tawi-Tawi",
+   "lat": 5.10473955,
+   "lng": 119.7401685
+ },
+ {
+   "Area": "Pikit, Cotabato",
+   "lat": 7.0537358,
+   "lng": 124.6722475
+ },
+ {
+   "Area": "Parang, Maguindanao",
+   "lat": 7.3756618,
+   "lng": 124.2671964
+ },
+ {
+   "Area": "Davao City",
+   "lat": 7.0644576,
+   "lng": 125.6083647
+ },
+ {
+   "Area": "Iligan City",
+   "lat": 8.2281556,
+   "lng": 124.2411508
+ },
+ {
+   "Area": "Malabang, Lanao del Sur",
+   "lat": 7.5964488,
+   "lng": 124.0735443
+ },
+ {
+   "Area": "Barangay San Antonio Village, Makati City",
+   "lat": 14.5356525,
+   "lng": 121.0148354
+ },
+ {
+   "Area": "Marawi City",
+   "lat": 8.00462,
+   "lng": 124.2854029
+ },
+ {
+   "Area": "Zamboanga City",
+   "lat": 6.9046004,
+   "lng": 122.0763695
+ },
+ {
+   "Area": " Kapatagan, Lanao del Sur",
+   "lat": 7.501154,
+   "lng": 124.1930994
+ },
+ {
+   "Area": "Barangay Poblacion, Mandaluyong City",
+   "lat": 14.58616885,
+   "lng": 121.0262185
+ },
+ {
+   "Area": "Jolo, Sulu",
+   "lat": 6.0536561,
+   "lng": 121.0002644
+ },
+ {
+   "Area": "Lamitan City, Basilan",
+   "lat": 6.6788439,
+   "lng": 122.101303
+ },
+ {
+   "Area": "Simunul, Tawi-Tawi",
+   "lat": 4.8368213,
+   "lng": 119.7984395
+ },
+ {
+   "Area": "Quezon City",
+   "lat": 14.6509905,
+   "lng": 121.0486155
+ },
+ {
+   "Area": "Kabasalan, Zamboanga Sibugay",
+   "lat": 7.7974574,
+   "lng": 122.7619459
+ },
+ {
+   "Area": "Pagadian City, Zamboanga del Sur",
+   "lat": 7.8249717,
+   "lng": 123.4365816
+ },
+ {
+   "Area": "South Upi, Maguindanao",
+   "lat": 6.8537883,
+   "lng": 124.1436
+ },
+ {
+   "Area": "Tagum City",
+   "lat": 7.4470234,
+   "lng": 125.8111811
+ },
+ {
+   "Area": "Barangay Culiat, Quezon City",
+   "lat": 14.6691216,
+   "lng": 121.055462
+ },
+ {
+   "Area": "Datu Salibo, Maguindanao",
+   "lat": 6.9998201,
+   "lng": 124.4855536
+ },
+ {
+   "Area": "Remedios T. Romualdez, Agusan del Norte",
+   "lat": 9.0518496,
+   "lng": 125.5850066
+ },
+ {
+   "Area": "Barangay Maharlika Village, Taguig City",
+   "lat": 14.4985715,
+   "lng": 121.0534637
+ },
+ {
+   "Area": "Barangay Lower Bicutan, Taguig City",
+   "lat": 14.4961942,
+   "lng": 121.056286
+ },
+ {
+   "Area": "Barangay 649, Manila City",
+   "lat": 14.5922972,
+   "lng": 120.9621646
+ },
+ {
+   "Area": "Baguio City",
+   "lat": 16.412007,
+   "lng": 120.5933937
+ }
+];
